9cf7058ab313dbea61c120efd2cb0d9d8bd9f6e2
Tan Duong <tan.duong@kamereo.vn>
2018-12-22 23:23:57 +0700

    Reformat code

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import styles from '../main.module.css';
 
 class About extends Component {
@@ -6,25 +6,27 @@ class About extends Component {
     return (
       <div>
         <div className={styles.maindetail}>
-      <div className={styles.about}>
-        <div className={styles.photo}>
-          <img src="https://images7.alphacoders.com/353/thumb-1920-353276.jpg" />
+          <div className={styles.about}>
+            <div className={styles.photo}>
+              <img src="https://images7.alphacoders.com/353/thumb-1920-353276.jpg" />
+            </div>
+            <div className={styles.text}>
+              <h2>About</h2>
+              <p>
+                - Revamped UI and app interface for customer-facing e-commerce
+                site with 200,000+ unique visitors per month
+                <br />- Designed and developed front-end for 20+ websites, using
+                jQuery, AJAX, RequireJS, and Handlebars.js
+                <br />- Created custom AngularJS components for internal
+                framework
+                <br />- Automated image optimization, using Grunt and minified
+                JS and CSS, which reduced page load times by up to 30%
+              </p>
+              <a href="#">Download CV</a>
+            </div>
+            <div className={styles.clear} />
+          </div>
         </div>
-        <div className={styles.text}>
-          <h2>About</h2>
-          <p>
-            - Revamped UI and app interface for customer-facing e-commerce site with 200,000+ unique visitors per month<br />
-            - Designed and developed front-end for 20+ websites, using jQuery, AJAX, RequireJS, and Handlebars.js<br />
-            - Created custom AngularJS components for internal framework<br />
-            - Automated image optimization, using Grunt and minified JS and CSS, which reduced page load times by up to
-            30%
-          </p>
-          <a href="#">Download CV</a>
-        </div>
-        <div className={styles.clear}>
-        </div>
-      </div>
-    </div>
       </div>
     );
   }

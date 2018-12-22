b8348509ae7ca55e5d901b14c246fd72bdcd604b
Tan Duong <tan.duong@kamereo.vn>
2018-12-22 23:04:18 +0700

    img tag has to have alt

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import styles from './main.module.css';
-import { NavLink } from "react-router-dom";
+import {NavLink} from 'react-router-dom';
 
 class Header extends Component {
   render() {
@@ -9,18 +9,31 @@ class Header extends Component {
         <div className={styles.navbar}>
           <div className={styles.menu}>
             <ul>
-              <li><NavLink to="/">HOME</NavLink></li>
-              <li><NavLink to="/about">ABOUT</NavLink></li>
-              <li><NavLink to="/skills">SKILLS</NavLink></li>
-              <li><NavLink to="/qualification">QUALIFICATION</NavLink></li>
-              <li><NavLink to="/contact">CONTACT</NavLink></li>
+              <li>
+                <NavLink to="/">HOME</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about">ABOUT</NavLink>
+              </li>
+              <li>
+                <NavLink to="/skills">SKILLS</NavLink>
+              </li>
+              <li>
+                <NavLink to="/qualification">QUALIFICATION</NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact">CONTACT</NavLink>
+              </li>
             </ul>
           </div>
         </div>
 
         <div className={styles.profile}>
           <div className={styles.home}>
-            <img src="https://upload.wikimedia.org/wikipedia/en/e/ee/Unknown-person.gif" />
+            <img
+              alt="avatar"
+              src="https://upload.wikimedia.org/wikipedia/en/e/ee/Unknown-person.gif"
+            />
             <h1>Trương Nghĩa</h1>
             <h2>Developer</h2>
           </div>
